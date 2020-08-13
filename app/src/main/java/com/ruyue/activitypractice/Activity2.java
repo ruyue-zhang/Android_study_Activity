802c0e5c90a4ae78af4e8b6a7ecc8f42ef3f5e1e
lpdft <772346730@qq.com>
2020-08-13 21:34:22 +0800

    [ruyue] feat:use implicit intent start contact

--- app/src/main/java/com/ruyue/activitypractice/Activity2.java
+++ app/src/main/java/com/ruyue/activitypractice/Activity2.java
@@ -2,13 +2,30 @@ package com.ruyue.activitypractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
+    static final int REQUEST_SELECT_CONTACT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        Button contacts = findViewById(R.id.choose);
+        contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+                }
+            }
+        });
     }
 }
