0a170a60e0fbd677e67b5edca5e1cd793a96591a
lpdft <772346730@qq.com>
2020-08-14 08:42:49 +0800

    [ruyue] feat:choose contact to display in Avtivity2

--- app/src/main/java/com/ruyue/activitypractice/Activity2.java
+++ app/src/main/java/com/ruyue/activitypractice/Activity2.java
@@ -1,15 +1,19 @@
 package com.ruyue.activitypractice;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
-    static final int REQUEST_SELECT_CONTACT = 1;
+    static final int PICK_CONTACT  = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,32 @@ public class Activity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
                 if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+                    startActivityForResult(intent, PICK_CONTACT);
                 }
             }
         });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        assert data != null;
+        Uri contactUri = data.getData();
+        assert contactUri != null;
+        Cursor cursor = getContentResolver().query(contactUri, null,
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            String userName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            TextView contact = findViewById(R.id.contact_info);
+            contact.setText(String.format("%s %s", userName, phoneNumber));
+        }
+
+        assert cursor != null;
+        cursor.close();
     }
 }
