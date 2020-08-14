db4da2970a2bcc3520f74958fb1a6a7900284499
lpdft <772346730@qq.com>
2020-08-14 09:11:11 +0800

    [ruyue] feat:add log to observe lifeCycle's status in logcat

--- app/src/main/java/com/ruyue/activitypractice/lifeCycle.java
+++ app/src/main/java/com/ruyue/activitypractice/lifeCycle.java
@@ -3,6 +3,7 @@ package com.ruyue.activitypractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class lifeCycle extends AppCompatActivity {
 
@@ -10,5 +11,43 @@ public class lifeCycle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_cycle);
+        Log.d("lifeCycle", "-----------onCreate----------");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifeCycle", "-----------onStart----------");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifeCycle", "-----------onResume----------");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifeCycle", "-----------onPause----------");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifeCycle", "-----------onStop----------");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifeCycle", "-----------onRestart----------");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifeCycle", "-----------onDestroy----------");
     }
 }
